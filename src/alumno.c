29a16e69eea348c683af354a0f7f7721bcec67fa
Joel Jassan <joeljassan@hotmail.com>
2023-04-15 19:53:01 -0300

    Construccion funcion "Serializar"

--- src/alumno.c
+++ src/alumno.c
@@ -45,9 +45,35 @@ static int SerializarNumero() {
 
 /*---  Public Function Implementation  --------------------------------------------------------- */
 
-int Serializar(const alumno_t * alumno, char cadena[], uint32_t espacio) {
-    return 0;
-}
+int Serializar(const alumno_t * alumno, char *cadena, uint32_t espacio) {
+    int disponibles = espacio;
+    int resultado;
+    int largo = 0;
+
+    *cadena = '{';   //cadena[0] = '{';  equivalentemente 
+
+    disponibles--;
+    resultado = SerializarCadena("apellido",&alumno->apellido, cadena, disponibles);
+
+    if (resultado > 0){
+        disponibles -= resultado;   //queda 1 espacio menos
+        cadena += resultado;    //desplaza 1 posicion de memoria
+        largo += resultado;
+        resultado = SerializarCadena("nombre",&alumno->nombre, cadena, disponibles);
+    }
+    if (resultado > 0){
+        disponibles -= resultado; 
+        cadena += resultado;
+        resultado = SerializarCadena("documento",alumno->documento, cadena, disponibles);
+    }
+    if(resultado > 0){
+        cadena += resultado;
+        *cadena = '}';  //cadena[espacio_total-espacio] = '}';  equivalentemente
+        resultado = espacio - disponibles;  //esto es posible, porque se hacen todos los 'if' si en algun punto 'SerializarCadena' = -1
+    }
+
+    return resultado;
+}   //Esta funcion hace {"apellido":apellido,"nombre":nombre,"documento":12345678}
 
 
 /*---  End of File  ---------------------------------------------------------------------------- */
